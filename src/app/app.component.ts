0839ddcfee30ee8d1d631042469de37bfa660737
Bas2544 <thebasza.2544@gmail.com>
2021-11-23 16:56:18 +0700

    Bs2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5;
+  n2: number = 6;
+  picName:string = "https://images.pexels.com/photos/10288317/pexels-photo-10288317.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" ;
+
+  hi(name:string){
+    alert("Hi" +name );
+  }
+
 }
+
